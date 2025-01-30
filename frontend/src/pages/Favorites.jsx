69b843ba75d63b58810b51b7873ce70ab7ae09ed
AmritSingh1502 <113255930+AmritSingh1502@users.noreply.github.com>
2025-01-30 14:38:46 +0530

    starting
    starting

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,11 @@
+import "../css/Favorites.css";
+function Favorites() {
+  return (
+    <div className="favorites-empty">
+        <h2>No Favorite Movies Yet</h2>
+        <p>Start adding movie to your F list</p>
+    </div>
+  )
+}
+
+export default Favorites
